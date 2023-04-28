a1735efb859aa2768fc6f0a97a7ca87d5ad8d82d
Inga Posiunaite <119749457+IngaPosiunaite@users.noreply.github.com>
2023-04-28 09:42:38 +0100

    Add files via upload

---
+++ src/Model/Basket.java
@@ -0,0 +1,162 @@
+package Model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import Util.DBConnector;
+
+public class Basket {
+	private Ticket currentTicket;
+	private ArrayList<Ticket> ticketsList = new ArrayList<>();
+	DBConnector db = new DBConnector();
+	private float totalToPay;
+
+	public Basket() {
+
+	}
+
+	public void getTicket(String ShowID, boolean isCircleSeat, int numberOfTicket, boolean isDiscount)
+			throws SQLException {
+		int number_of_avaliable_ticket = 0;
+		db.connect();
+		String sql = "SELECT COUNT(isCircleSeat) FROM tickets WHERE isCircleSeat =" + isCircleSeat + " and showID = "
+				+ ShowID + " AND isSold = false";
+		ResultSet rs = db.runQuery(sql);
+		while (rs.next()) {
+			number_of_avaliable_ticket = rs.getInt("COUNT(isCircleSeat)");
+			if (number_of_avaliable_ticket >= numberOfTicket) // enough tickets for sell
+			{
+
+				// get next availavle tick by showID
+
+				String sql_getTicketID = "SELECT  MIN(TicketID) From tickets Where isCircleSeat = " + isCircleSeat
+						+ " AND SHOWID = '" + ShowID + "' AND isSold = false";
+				ResultSet rs_checkID = db.runQuery(sql_getTicketID);
+				while (rs_checkID.next()) {
+					int currentTicketID = rs_checkID.getInt("MIN(TicketID)");
+					// System.out.println("current Ticket ID = " + currentTicketID);
+					for (int i = 0; i < numberOfTicket; i++) {
+						Ticket currentTicket = new Ticket(currentTicketID + i);
+						currentTicket.setDiscount(isDiscount);
+						ticketsList.add(currentTicket);
+					}
+				}
+			} else // not enough
+			{
+				System.out.println("insufficient tickets available, please select other times or show.");
+			}
+		}
+		db.close();
+	}
+
+	// find the available ticket, and put it into basket, when the customer pay,
+	// change isSold to true and assign booking ID.
+	// get show id by calling the ticket ID, and input the details base on the id
+	// import information but not
+
+	public void addTicketIntoBasket(String ShowID, boolean isCircleSeat, int numberOfTicket, boolean isDiscount)
+			throws SQLException // db connecting
+	{
+		db.connect();
+		for (int i = 0; i < numberOfTicket; i++) {
+			Ticket currentTicket = new Ticket(ShowID);
+			int currentTicketID = 0;
+			String sql_getTicketID = "SELECT  MIN(TicketID) From tickets Where isCircleSeat = " + isCircleSeat
+					+ " AND SHOWID = '" + ShowID + "' AND isSold = false";
+			ResultSet rs_checkID = db.runQuery(sql_getTicketID);
+			while (rs_checkID.next()) {
+				currentTicketID = rs_checkID.getInt("MIN(TicketID)");
+				System.out.println(currentTicketID);
+				currentTicketID = currentTicketID + i;
+				System.out.println("	currentTicket ID :" + currentTicketID);
+				currentTicket.getTicketDetailsByTicketID(currentTicketID);
+				System.out.println(currentTicketID + "add to basket");
+				ticketsList.add(currentTicket);
+			}
+			System.out.println(ticketsList.size());
+		}
+		db.close();
+	}
+
+	public void printBasket() // print out the ticket already added to basket
+	{
+		if (ticketsList.size() > 0) {
+			int i = 0;
+			for (Ticket t : ticketsList) {
+				// System.out.println(t.getTicketNumber());
+				i = i + 1; // count the order of tickets.
+				System.out.println("===== Item #" + i + " in basket" + "=====" + "\n");
+				System.out.println("Show Title: " + t.getTitleOfShow());
+				System.out.println("Date :" + t.getDate());
+				if ((t.getIsCircleSeat() == true)) {
+					System.out.println("Seat Type: Circle Seat");
+				} else {
+					System.out.println("Seat Type: Stall Seat");
+				}
+				if ((t.getIsDiscount()) == false) {
+					System.out.println("Full Fare Ticket");
+				} else {
+					System.out.println("Concession Fares Ticket");
+				}
+				System.out.println();
+				System.out.println("============================");
+			}
+			float total_to_return = getTotalPrice();
+			System.out.println("The total cost is :" + total_to_return);
+			totalToPay = total_to_return;
+
+		} else // no ticket
+		{
+			System.out.println("No ticket in basket");
+		}
+
+	}
+
+	public float getTotalPrice() {
+		float total_to_return = 0;
+		for (Ticket t : ticketsList) {
+			total_to_return = total_to_return + t.getPrice();
+		}
+		return total_to_return;
+	}
+
+	public float getTotal() {
+		return totalToPay;
+	}
+
+	public void updateAllTicketByPost(boolean input_isBypost) {
+		db.connect();
+		for (Ticket t : ticketsList) {
+			t.setByPost(input_isBypost);
+		}
+		db.close();
+	}
+
+	public void emptyBasket() {
+		ticketsList.clear();
+		totalToPay = 0;
+	}
+
+	public boolean getAnyTicketsInBasketDiscount() {
+		boolean toBeReturned = false;
+		for (Ticket t : ticketsList) {
+			if (t.getIsDiscount() == true) {
+				toBeReturned = true;
+			}
+		}
+		return toBeReturned;
+	}
+
+	public void setPostFee() {
+		totalToPay = totalToPay + 1;
+	}
+
+	public void processPayment() throws SQLException {
+		for (Ticket t : ticketsList) {
+			t.updateSold();
+		}
+		emptyBasket();
+	}
+
+}
---
+++ src/Model/MusicalShow.java
@@ -0,0 +1,45 @@
+package Model;
+
+import java.sql.SQLException;
+
+public class MusicalShow extends Show implements Language, MusicalAccompaniment {
+
+	public MusicalShow(String titleOfShow, String typeOfShow, int showDuration, String description, int SeatsStall,
+			int SeatsCircle, float stallPrice, float circlePrice, String showDate, String showTime) throws SQLException {
+		super(titleOfShow, typeOfShow, showDuration, description, SeatsStall, SeatsCircle, stallPrice, circlePrice,
+				showDate, showTime);
+typeOfShow = "Music Show";
+
+
+
+	}
+	
+	public MusicalShow(String titleOfShow, String typeOfShow, int showDuration, String description, int SeatsStall,
+			int SeatsCircle, float stallPrice, float circlePrice, String showDate, String showTime, String liveMusic) throws SQLException {
+		super(titleOfShow, typeOfShow, showDuration, description, SeatsStall, SeatsCircle, stallPrice, circlePrice,
+				showDate, showTime,  liveMusic );
+typeOfShow = "Music Show";
+
+
+
+	}
+
+	@Override
+	public void showPrinciplePerformance() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setLanguage(String inputLanguage) {
+		language = inputLanguage;
+
+	}
+
+	@Override
+	public void setMusicalAccompaniment(String inputIsMusicalAccompaniment) {
+liveMusic = inputIsMusicalAccompaniment;
+		
+	}
+
+}
