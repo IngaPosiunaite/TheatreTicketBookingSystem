722466fc9c2f82fbee6348f5f7194f699d5a0408
Inga Posiunaite <119749457+IngaPosiunaite@users.noreply.github.com>
2023-05-19 13:57:33 +0100

    Add files via upload

---
+++ src/Controller/Controller.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import java.sql.ResultSet;
+
+import Util.DBConnector;
+import Util.InputReader;
+
+public class Controller {
+	private static InputReader reader;
+	private static DBConnector c;
+
+	public Controller() {
+
+	}
+
+	public static void printWelcome() {
+		System.out.println("Welcome to the Theatre Royal");
+
+	}
+
+	public static void printBye() {
+		System.out.println("Thank you. See you again soon!");
+	}
+
+}
